39e84ab00bdd98a528e086b631a523962f6e7faf
9ce0558b <9ce0558b@9ce0558b.com>
2021-01-06 11:21:11 +0800

    init

---
+++ src/README.md
@@ -0,0 +1,26 @@
+# Multi-Highlight
+
+Efficiently mark multiple words in a page.
+
+20191028:
+- Customizable window size
+- Add 4 more colors (now contains 10 colors)
+
+20190609:
+- Add "paste keywords to new pages" mode
+
+20190503:
+- Minimize extension size
+
+20190110:
+- Highlight any strings (not just full word)
+- Add Instant Search (highlight on-the-fly)
+- Add customizable delimiter
+- Make Highlight Words individual to each tab
+- UI changes
+- Add Ctrl+Shift+F as default shortcut
+
+20181231:
+- Organize code
+- Change UI (based on Multi-Highlight)
+- Change extension icon
---
+++ src/popup.js
@@ -0,0 +1,55 @@
+// ************************************************************************
+// Multi Highlight popup js
+// ************************************************************************
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        var currTab = tabs[0];
+        if (currTab) { // Sanity check
+            var tabkey = get_tabkey(currTab.id);
+            chrome.storage.local.get(['settings', tabkey], function (result) {
+                // fetch general settings
+                var settings = result.settings;
+                var tabinfo = result[tabkey];
+                var flag = {"is_change": false};
+                // init popup interface
+                container.style.width = settings.popup_width + "px";
+                highlightWords.style.minHeight = settings.popup_height + "px";
+                // init popup values
+                delimiter.value = settings.delim;
+                instant.checked = settings.isInstant;
+                saveWords.checked = settings.isSaveKws;
+                if (settings.isSaveKws && tabinfo.isNewPage) { // decide which keywords to fill in
+                    flag.is_change = true;
+                    highlightWords.value = settings.latest_str;
+                } else {
+                    highlightWords.value = settings.latest_str;
+                }
+                highlightWords.value += highlightWords.value ? settings.delim : "";
+                tabinfo.isNewPage = false;
+                chrome.storage.local.set({[tabkey]: tabinfo}, function () {
+                    if (flag.is_change) {
+                        handle_highlightWords_change(tabkey);
+                    }
+                });
+                $("#highlightWords").select()
+
+                // register listener
+                $("#delimiter").on("input", function () {
+                    handle_delimiter_change(tabkey, settings);
+                })
+                $("#highlightWords").on("input", function () {
+                    handle_highlightWords_change(tabkey);
+                })
+                $("#instant").on("input", function () {
+                    handle_instant_mode_change(settings);
+                })
+                $("#saveWords").on("input", function () {
+                    handle_saveWords_mode_change(settings);
+                })
+
+            });
+        }
+    });
+});
